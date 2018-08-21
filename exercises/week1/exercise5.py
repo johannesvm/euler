146557fa23fefc3f551699ebf183b9259cc1af08
Johannes V. Meo <johannesv.meo@Johanness-MacBook-Air.local>
2018-08-21 21:21:54 +0200

    Completed exercise5

--- exercises/week1/exercise5.py
+++ exercises/week1/exercise5.py
@@ -1,3 +1,17 @@
 
 with open('names.txt', 'r') as f:
+    string = f.read()
+    names = string.split(',')
+    for i in range( len(names)):
+        names[i] = names[i][1:-1]
+    names = sorted(names)
     
+    scores = []
+    for name in names:
+        score = 0
+        for letter in name:
+            score += ( ord(letter) - 64)
+        scores.append(score)
+        
+    for i in range( len(names)):
+        scores[i] *= (i+1)
