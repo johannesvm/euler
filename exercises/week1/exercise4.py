8a40814ad9ed3094b4fc421385eaca4354ca0e58
Johannes V. Meo <johannesv.meo@1x-193-157-243-111.uio.no>
2018-08-23 10:36:08 +0200

    Change in exercise4.py

--- exercises/week1/exercise4.py
+++ exercises/week1/exercise4.py
@@ -4,20 +4,10 @@ def factorize(n):
     p = 2
     while n >= p:
         if n % p == 0:
-            print(f, n, p)
             f.append(p)
             if n/p == 1:
                 break;
             n = n/p
         else:
             p += 1
-            while True:
-                if p % 2 == 0:
-                    p += 1
-                else:
-                    break;
-    return f
-
-a = factorize(29)
-print(a)
-            
+    return f  
